d9255f8d66a29e700768fc830d1c322fcd7f88fd
indality <indans112@gmail.com>
2026-09-01 15:52:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,5 +7,6 @@ export const firebaseConfig = {
   projectId: "disku-golfs",
   storageBucket: "disku-golfs.firebasestorage.app",
   messagingSenderId: "13466680530",
-  appId: "1:13466680530:web:b5e31e08866679a48dc10b"
+  appId: "1:13466680530:web:b5e31e08866679a48dc10b",
+    databaseURL: "https://disku-golfs-default-rtdb.europe-west1.firebasedatabase.app"
 };
